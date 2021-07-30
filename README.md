3633445d109d185c81c0be62e8e0226be225282f
Amandeep Singh <amandeep.bajwa@care.com>
2021-07-31 03:43:18 +0530

    fix: static

--- README.md
+++
@@ -1,5 +0,0 @@
-# Gurbani App
-
-Display Shabad with links to Audio/Video Shabads
-
-Thanks to https://www.sikhitothemax.org/ for online Gurbani Search Sewa
---
+++ _next/static/4Wn4HQ0IVJ9-C6hDqXP03/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-30bb6be301b521123d45.js"],"/_error":["static/chunks/pages/_error-157366ffea25803f02fd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
