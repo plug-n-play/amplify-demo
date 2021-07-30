160a7c9d6d19d5677d161fc57f78f8cca3069e0e
Amandeep Singh <amandeep.bajwa@care.com>
2021-07-31 03:25:14 +0530

    feat: initial commit

---
+++ utils/list.js
@@ -0,0 +1,208 @@
+module.exports = [
+  {
+    "id": 4648,
+    "title": {
+      "unicode": "ਬਾਬੀਹਾ ਅੰਮ੍ਰਿਤ ਵੇਲੈ ਬੋਲਿਆ ਤਾਂ ਦਰਿ ਸੁਣੀ ਪੁਕਾਰ ॥",
+      "transliteration": "baabeehaa a(n)mirat velai boliaa taa(n) dhar sunee pukaar ||"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Nirmal Singh Ji Khalsa",
+        "url": "https://gaana.com/album/amrit-vele-boliya",
+        "duration": "05:21"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=4648&q=babiha%20amrit%20vele%20boleya&type=4&source=all&highlight=54949"
+  },
+  {
+    "id": 2218,
+    "title": {
+      "unicode": "ਸੋ ਸਤਿਗੁਰੁ ਪਿਆਰਾ ਮੇਰੈ ਨਾਲਿ ਹੈ",
+      "transliteration": "so satigur piaaraa merai naal hai"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Lakhwinder Singh Ji (Hazoori Ragi Sri Darbar Sahib-Amritsar)",
+        "url": "https://www.youtube.com/watch?v=oyxxNGvl1RA",
+        "duration": "07:59"
+      },
+      {
+        "name": "Bhai Davinder Singh Ji (Batala)",
+        "url": "https://www.youtube.com/watch?v=QIc3akHq2Mc",
+        "duration": "07:39"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=2218&q=So%20Satgur%20Pyara%20Merai%20Naal%20Hai&type=4&source=all&highlight=25641"
+  },
+  {
+    "id": 1575,
+    "title": {
+      "unicode": "ਸਤਿਗੁਰ ਬਚਨ ਤੁਮ੍ਹਾਰੇ",
+      "transliteration": "satigur bachan tum(h)aare"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Ravinder Singh Ji (Hazoori Ragi Sri Darbar Sahib-Amritsar)",
+        "url": "https://www.youtube.com/watch?v=VT_PmxrH2eo",
+        "duration": "09:48"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=1575",
+    "todo": "convert into array and add https://www.youtube.com/watch?v=TN-LIULu3Fk&list=RDTN-LIULu3Fk&start_radio=1"
+  },
+  {
+    "id": 1077,
+    "title": {
+      "unicode": "ਧੰਨੁ ਧੰਨੁ ਧੰਨੁ ਜਨੁ ਆਇਆ",
+      "transliteration": "dha(n)n dha(n)n dha(n)n jan aaiaa"
+    },
+    "keertans": [
+      {
+        "name": "Sant Sujan Singh Ji",
+        "url": "https://youtu.be/sgZQbMK5iRI?t=33028",
+        "duration": ""
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=1077&q=Dhan%20Dhan%20Dhan%20jan%20aaya&type=4&source=all&highlight=13423"
+  },
+  {
+    "id": 2988,
+    "title": {
+      "unicode": "ਤੈ ਸਾਹਿਬ ਕੀ ਮੈ ਸਾਰ ਨ ਜਾਨੀ ॥",
+      "transliteration": "tai saahib kee mai saar na jaanee ||"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Anand Prakash Singh Ji",
+        "url": "https://youtu.be/xsSDhe9N66M?t=222",
+        "duration": ""
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=2988&q=tein%20saheb%20ki%20main&type=4&source=all&highlight=33762"
+  },
+  {
+    "id": 40232,
+    "title": {
+      "unicode": "ਰਾਮ ਕਬੀਰੈ ਭੇਦੁ ਨ ਭਾਈ",
+      "transliteration": "raam kabeerai bhedh na bhaiee"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Gurnam Singh/Deedar Singh (Delhi)",
+        "url": "https://www.youtube.com/watch?v=ONEHVT5Ce3w",
+        "duration": "08:05"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=40232&q=raam%20kabeerai%20bhedh%20na%20bhaiee&type=4&source=all&highlight=201944"
+  },{
+    "id": 2370,
+    "title": {
+      "unicode": "ਪ੍ਰਭ ਡੋਰੀ ਹਾਥਿ ਤੁਮਾਰੇ ॥",
+      "transliteration": "prabh ddoree haath tumaare ||"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Jaswant Singh - Live Sri Harmandir Sahib",
+        "url": "https://www.youtube.com/watch?v=8Pz8WglBe8E",
+        "duration": "12:35"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=2370&q=soi%20karawe%20jo%20tudh%20bhave&type=4&source=all&highlight=27146"
+  },
+  {
+    "id": 1524,
+    "title": {
+      "unicode": "ਮੋਹਿ ਗਰੀਬ ਕਉ ਲੇਹੁ ਰਲਾਇ ॥",
+      "transliteration": "moh gareeb kau leh ralai ||"
+    },
+    "keertans": [
+      {
+        "name": "Bibi Harleen Kaur Ji Khalsa",
+        "url": "https://www.youtube.com/watch?v=pVYbVUFsS5o",
+        "duration": "19:44"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=1524&q=mohe%20gareeb%20ko%20leho&type=4&source=all&highlight=17961"
+  },
+  {
+    "id": 40023,
+    "title": {
+      "unicode": "ਕਲਿ ਤਾਰਣਿ ਗੁਰੁ ਨਾਨਕੁ ਆਇਆ ॥੨੩॥",
+      "transliteration": "kal taaran gur naanak aaiaa ||23||"
+    },
+    "keertans": [
+      {
+        "name": "Dr. Alankar Singh",
+        "url": "https://www.youtube.com/watch?v=2PWkviN010g",
+        "duration": "18:15"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=40023&q=kal%20taaran%20guru%20nanak&type=4&source=all&highlight=200206"
+  },
+  {
+    "id": 40549,
+    "title": {
+      "unicode": "ਗੁਰ ਅਰਜਨ ਵਿਟਹੁ ਕੁਰਬਾਣੀ ॥੨੩॥",
+      "transliteration": "gur arajan viTahu kurabaanee ||23||"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Maninder Singh Ji 10 June 2018",
+        "url": "https://www.youtube.com/watch?v=EzD1JAbmV_w",
+        "duration": "10:12"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=40549&q=guru%20arjun%20vitoh%20kurbani&type=4&source=all&highlight=204483"
+  },
+  {
+    "id": 33650,
+    "title": {
+      "unicode": "ਮੈ ਤੁਝ ਬਿਨੁ ਬੇਲੀ ਕੋ ਨਹੀ ਤੂ ਅੰਤਿ ਸਖਾਈ ॥",
+      "transliteration": "mai tujh bin belee ko nahee too a(n)t sakhaiee ||"
+    },
+    "keertans": [
+      {
+        "name": "Sant Sujan Singh Ji",
+        "url": "https://youtu.be/LE7oCQl9lyI?t=3681",
+        "duration": ""
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=2979&q=mqbb&type=1&source=all&highlight=33650"
+  },
+  {
+    "id": 30993,
+    "title": {
+      "unicode": "ਕਰਿ ਬੰਦੇ ਤੂ ਬੰਦਗੀ ਜਿਚਰੁ ਘਟ ਮਹਿ ਸਾਹੁ",
+      "transliteration": "kar ba(n)dhe too ba(n)dhagee jichar ghaT meh saahu"
+    },
+    "keertans": [
+      {
+        "name": "Bhai Nirmal Singh Ji Khalsa",
+        "url": "https://www.youtube.com/watch?v=UTiAvWZxO2w",
+        "duration": "14:39"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=2772&q=kbqb&type=1&source=all&highlight=30993"
+  },
+  {
+    "id": 24681,
+    "title": {
+      "unicode": "ਅਤਿ ਊਚਾ ਤਾ ਕਾ ਦਰਬਾਰਾ ॥",
+      "transliteration": "at uoochaa taa kaa dharabaaraa ||"
+    },
+    "keertans": [
+      {
+        "name": "Sant Sujan Singh Ji",
+        "url": "https://www.youtube.com/watch?v=UnauKqSTgbg",
+        "duration": "14:39"
+      }
+    ],
+    "completeShabadURL": "https://www.sikhitothemax.org/shabad?id=2159&q=Aaq&type=1&source=all&highlight=24681"
+  }
+]
+// https://www.sikhitothemax.org/shabad?id=40035&q=ek%20baba%20akal%20roop&type=4&source=all&highlight=200308
+// ਇਕੁ ਬਾਬਾ ਅਕਾਲ ਰੂਪੁ ਦੂਜਾ ਰਬਾਬੀ ਮਰਦਾਨਾ।
+// eik baabaa akaal roop dhoojaa rabaabee maradhaanaa|
+
+// https://www.youtube.com/watch?v=_Ej6Tio_-kE&feature=youtu.be
+// TODO: Add Nitnem
